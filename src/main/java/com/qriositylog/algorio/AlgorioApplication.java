9938b624bab79e1cf5bfd89064265092cc1d4ddb
Queue-ri <qriositylog@gmail.com>
2022-03-31 17:44:34 +0900

    chore(deps): Add initial dependencies
    - Spring Web
    - Spring Data JPA
    - Lombok

---
+++ src/main/java/com/qriositylog/algorio/AlgorioApplication.java
@@ -0,0 +1,13 @@
+package com.qriositylog.algorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qriositylog/algorio/AlgorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qriositylog.algorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
