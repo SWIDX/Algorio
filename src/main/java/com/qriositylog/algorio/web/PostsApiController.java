88c63f584d91dca2ad3729d150b5d623e9864145
Queue-ri <qriositylog@gmail.com>
2022-04-08 22:11:14 +0900

    style: Add slash before request url

--- src/main/java/com/qriositylog/algorio/web/PostsApiController.java
+++ src/main/java/com/qriositylog/algorio/web/PostsApiController.java
@@ -13,19 +13,19 @@ public class PostsApiController {
     private final PostsService postsService;
 
     // 등록
-    @PostMapping("api/v1/posts")
+    @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
 
     // 수정
-    @PutMapping("api/v1/posts/{id}")
+    @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
 
     // 조회
-    @GetMapping("api/v1/posts/{id}")
+    @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
     }
