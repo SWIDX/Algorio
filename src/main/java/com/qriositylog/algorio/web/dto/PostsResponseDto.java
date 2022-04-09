0fd4ab22174ee52c063218bcdb7cd91a21ba0b54
Queue-ri <qriositylog@gmail.com>
2022-04-09 20:40:42 +0900

    feat: Change tag type in response dto

--- src/main/java/com/qriositylog/algorio/web/dto/PostsResponseDto.java
+++ src/main/java/com/qriositylog/algorio/web/dto/PostsResponseDto.java
@@ -3,19 +3,21 @@ package com.qriositylog.algorio.web.dto;
 import com.qriositylog.algorio.domain.posts.Posts;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class PostsResponseDto {
     private Long id;
     private String title;
     private String content;
     private String author;
-    private String tag;
+    private List<String> tags;
 
-    public PostsResponseDto(Posts entity) {
+    public PostsResponseDto(Posts entity, List<String> tagList) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.content = entity.getContent();
         this.author = entity.getAuthor();
-        this.tag = entity.getTag();
+        this.tags = tagList;
     }
 }
